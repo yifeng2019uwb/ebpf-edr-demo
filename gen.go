9fda07fb91ef996522d940d010bce04c0992d3f8
yifeng2019uwb <yifeng2019@gmail.com>
2026-04-18 03:46:29 +0000

    generate Go wrappers

--- gen.go
+++ gen.go
@@ -1,5 +1,5 @@
-//go:generate go tool bpf2go -tags linux process kernel/execsnoop.bpf.c
-//go:generate go tool bpf2go -tags linux network kernel/networkmonitor.bpf.c
-//go:generate go tool bpf2go -tags linux file kernel/opensnoop.bpf.c
+//go:generate go tool bpf2go -tags linux -cflags "-I../bpf-developer-tutorial/src/third_party/vmlinux/x86 -I/usr/include/bpf" process kernel/execsnoop.bpf.c
+//go:generate go tool bpf2go -tags linux -cflags "-I../bpf-developer-tutorial/src/third_party/vmlinux/x86 -I/usr/include/bpf" file kernel/opensnoop.bpf.c
+//go:generate go tool bpf2go -tags linux -target amd64 -cflags "-I../bpf-developer-tutorial/src/third_party/vmlinux/x86 -I/usr/include/bpf" tcp kernel/tcpconnlat.bpf.c
 
 package main
---
+++ go.mod
@@ -0,0 +1,10 @@
+module ebpf-edr-demo
+
+go 1.24.2
+
+require (
+	github.com/cilium/ebpf v0.21.0 // indirect
+	golang.org/x/sys v0.37.0 // indirect
+)
+
+tool github.com/cilium/ebpf/cmd/bpf2go
